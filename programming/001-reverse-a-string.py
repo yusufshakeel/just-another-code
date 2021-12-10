fe29b91b1cfc9e44a81bdc88570738d9d8040199
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-12-10 23:35:43 +0530

    string reverse

---
+++ programming/001-reverse-a-string.py
@@ -0,0 +1,12 @@
+s = ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
+
+print('before', s)
+
+start = 0
+end = len(s) - 1
+while start < end:
+    s[start], s[end] = s[end], s[start]
+    start += 1
+    end -= 1
+
+print('after', s)
